0a8e423821b17ab629603c6e0e9c8f6a6c491beb
Luciano655dev <lucianomenezes655@gmail.com>
2025-04-21 03:27:39 -0400

    fifififiif

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import Main from "./components/Main/main"
+import Main from "./components/Main/Main"
 import Info from "./components/Info/info"
 import Game from "./components/Game/game"
 import Footer from "./components/Footer/footer"
